ed1b2f182b1987c0b1282c8a8d6f9e39aaba2abf
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-04-17 19:43:35 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,107 +1,39 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { getImagesByQuery } from './js/pixabay-api';
-import {
-  createGallery,
-  clearGallery,
-  showLoader,
-  hideLoader,
-} from './js/render-functions';
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
-const form = document.querySelector('.form');
-const searchInput = document.querySelector('.input');
-const loadMore = document.querySelector('.js-load-more');
-
-let page = 1;
-let enteredInput = '';
-
-form.addEventListener('submit', handleSubmit);
-loadMore.addEventListener('click', onLoadMore);
-loadMore.classList.replace('js-load-more', 'load-more-hidden');
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  enteredInput = searchInput.value.trim();
-  page = 1;
-
-  if (!enteredInput) {
-    iziToast.warning({
-      position: 'topRight',
-      title: 'Warning',
-      message: 'Please enter a search query',
-    });
-    searchInput.focus();
-    return;
-  }
-
-  showLoader();
-  clearGallery();
-  loadMore.classList.add('load-more-hidden');
-
-  try {
-    const data = await getImagesByQuery(enteredInput, page);
-
-    if (!data.hits || data.hits.length === 0) {
-      iziToast.warning({
-        position: 'topRight',
-        title: 'Warning',
-        message: 'Sorry, no images found. Please try another query!',
-      });
-      return;
-    }
-
-    createGallery(data.hits);
-
-    const totalPages = Math.ceil(data.totalHits / 15);
-    if (page < totalPages) {
-      loadMore.classList.remove('load-more-hidden');
-      loadMore.classList.add('js-load-more');
-    }
-  } catch (error) {
-    iziToast.error({
-      position: 'topRight',
-      title: 'Error',
-      message: 'Failed to fetch images. Please try again later.',
-    });
-    console.error('Error:', error);
-  } finally {
-    hideLoader();
-    searchInput.value = '';
-  }
-}
-
-async function onLoadMore() {
-  page++;
-  loadMore.disabled = true;
-  loadMore.classList.add('load-more-hidden');
-  showLoader();
-
-  try {
-    const data = await getImagesByQuery(enteredInput, page);
-    const totalPages = Math.ceil(data.totalHits / 15);
-
-    if (page > totalPages) {
-      iziToast.info({
-        position: 'topRight',
-        message: "We're sorry, but you've reached the end of search results.",
-      });
-      return;
-    }
-
-    createGallery(data.hits);
-
-    if (page < totalPages) {
-      loadMore.classList.remove('load-more-hidden');
-      loadMore.classList.add('js-load-more');
-    }
-  } catch (error) {
-    iziToast.error({
-      position: 'topRight',
-      title: 'Error',
-      message: error.message,
-    });
-  } finally {
-    hideLoader();
-    loadMore.disabled = false;
-  }
-}
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    base: '/goit-js-HW11/',
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
