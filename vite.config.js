786b68930b736d113c02f1a294c241fb804562ec
vvvemanresuaretne <vvvasilkovskyi@gmail.com>
2025-04-17 19:49:05 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,38 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import fg from 'fast-glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
+const inputFiles = fg.sync(['./src/*.html']);
+
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-HW11/',
+    root: 'src',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    base: '/goit-js-HW11/',
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./*.html'), // ищет html в корне
+        input: inputFiles.length ? inputFiles : './src/index.html',
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
       },
-      outDir: 'dist',
+      outDir: '../dist',
       emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
-      FullReload(['./**/*.html']),
+      FullReload(['./src/**/*.html']),
       SortCss({
         sort: 'mobile-first',
       }),
